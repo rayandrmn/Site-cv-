688eeb9aab71298da64bd5564ac314e3daf73e91
rayandrmn <99180225+rayandrmn@users.noreply.github.com>
2022-05-09 08:31:22 +0200

    Add files via upload

--- site cv stage/main.js
+++ site cv stage/main.js
@@ -53,9 +53,9 @@
 
         //Fusee();
         //gravité();
+ 
 
-
-
+         
 
 
 
@@ -82,6 +82,23 @@
         
                 event.preventDefault();
                 var hash = this.hash;
+                fuseeposition = 300;
+                fusee.style.top = `${fuseeposition}%`;
+                 lol = true;
+        
+                $('body,html').animate({scrollTop: $(hash).offset().top} , 2000, function (){window.location.hash = hash;})
+
+                console.log(fuseeposition);  
+
+                 
+        
+            });
+
+
+            $("#contacts").on("click", function(event){
+        
+                event.preventDefault();
+                var hash = this.hash;
                 fuseeposition = 400;
                 fusee.style.top = `${fuseeposition}%`;
                  lol = true;
@@ -93,6 +110,52 @@
                  
         
             });
+
+             
+
+            $(".bouton").click(function(){
+                let nom = $("#inputnom").val();
+                console.log(nom);
+                var mailregex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                let mail = $("#inputemail").val();
+                let text =$("#inputtext").val();
+                if(nom == ""){
+                    $("#nom, #inputnom").css("background-color", "#cc3300");
+                    console.log("faut mettre ton nom");
+                }else{
+                    $("#nom, #inputnom").css("background-color", "rgb(106, 165, 200)");
+                    console.log("nom correcte");
+                    console.log(text);
+                }
+
+
+                if(mailregex.test(mail) == true){
+                    console.log("nice");
+                    $("#email, #inputemail").css("background-color", "rgb(106, 165, 200)");
+                    console.log("emal correcte")
+                }else{
+                    $("#email, #inputemail").css("background-color", "#cc3300")
+                    console.log("email faux");
+                }
+
+
+                if(text == ""){
+                    $("#text, #inputtext").css("background-color", "#cc3300");
+                    console.log("faut remplir la le champs");
+                }else{
+                    $("#text, #inputtext").css("background-color", "rgb(106, 165, 200)");
+                    console.log("champ remplit correcte")
+                }
+                return false;
+            })
+
+           
+
+            $(".contener-input").click(function(){
+                $(".contener-input").css("width", "80%");
+
+                $(this).css("width", "100%");
+             })
             
           })
 
@@ -114,6 +177,8 @@ function apparitionid(){
 
 
 
-   
+var canvas = document.querySelector('.myCanvas');
+var width = canvas.width = 320;
+var height = canvas.height = 240;
 
         });
